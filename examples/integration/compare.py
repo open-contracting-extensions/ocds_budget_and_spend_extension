0991695586820e2fc8b3c3d9e6cd497bec3dbe17
James McKinney <james@slashpoundbang.com>
2018-12-19 17:26:01 -0500

    Tidy examples/integration/compare.py

--- examples/integration/compare.py
+++ examples/integration/compare.py
@@ -2,19 +2,19 @@
 This file is part of a worked example to show how OCDS data can be compared to
 data published using the Fiscal Data Package.
 
-In this example, we take a number of shortcuts: pre-preparing a flat version of
+In this example, we take a number of shortcuts: preparing a flat version of
 our OCDS data, and reading the underlying FDP tables directly.
 
 A full implementation would:
 
-* Read OCDS data directly;
+* Read OCDS data directly
 * Use datapackage.json to understand the logical model of an FDP file
-
 """
+
 import pandas as pd
 
-fdp = pd.read_csv("fdp/budget-and-spend.csv")
-ocds = pd.read_csv("financialProgress-extract.csv")
+fdp = pd.read_csv('fdp/budget-and-spend.csv')
+ocds = pd.read_csv('financialProgress-extract.csv')
 
 merged = pd.merge(ocds, fdp, how='left',
                   left_on=[
@@ -32,4 +32,4 @@ merged = pd.merge(ocds, fdp, how='left',
                       'Func',
                   ])
 
-merged.to_csv("budget-contracting-compared.csv", index=False)
+merged.to_csv('budget-contracting-compared.csv', index=False)
